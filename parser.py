08d30f50a6d6826d8357a674276a68d947d561ce
JohnTheTechie <janakiraman3394@gmail.com>
2019-08-03 21:18:08 +0530

    improvement added

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
 
-import xml.etree.ElementTree as ET
 import logging
-import sys
 import os.path as fpath
+import sys
+import xml.etree.ElementTree as ET
+
 
 class TextParser:
     """
@@ -10,16 +11,34 @@ class TextParser:
     developers using this class must implement find_type()
     """
 
+    class __TextFileIterator:
+        """
+        iterator class for file contents.
+        returns contents line by line
+        """
+
+        def __init__(self, file):
+            self.file = file
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            return self.file.readline()
+
+
     def __init__(self, file):
         """
         tags_level is a dictionary containing all tags for various topic levels
         tags_content is a dictionary containing tags for various content types/classes
 
+        :param file : location of the file to be parsed
+
         """
         self.tags_level = {}
         self.tags_content = {}
         self.attributes = {}
-        self.file_for_parsing = None
+        self.file_for_parsing = self._get_file_to_parse(file)
         logging.basicConfig(filename="./logs/running_log.log", level=logging.INFO)
 
     def set_tag_for_level(self, level, tag):
@@ -57,8 +76,9 @@ class TextParser:
     def find_type(self, text):
         """
         must be implemented before usage. raise UnimplementedFunctionError.
+        to break the text, dev can use _parse_text() function
         :param text:
-        :return:
+        :return: (tag, content, **kwargs), kwargs should specify the attributes for the tag
         """
         raise UnimplementedFunctionError("implement the function first")
 
@@ -124,9 +144,35 @@ class TextParser:
         tree = ET.ElementTree(root_element)
         logging.info(f"tree has been created with {root_element} as root")
 
+    def _create_element(self, super_element, element_tag, content, **kwargs):
+        """
+        creates a subelement for the given super element with the content.
+        Attributes should be provided via kwargs
+        :param super_element: elementTree.element object
+        :param element_tag: string literal, name of the tag
+        :param content: content to be added to tag
+        :param kwargs: attributes should be added from the self.attributes item
+        :return: None
+        """
+        # TODO: finish the function
 
+    def _parse_text(self, text="", seperator=' '):
+        """
+        seperate the input text and return a list of words
+        :param text: line read from the file
+        :param seperator: literal at which the text string needs to be split
+        :return: list of words
+        """
+        list_words = text.split(seperator)
+        logging.info(f"line parsed into {list_words}")
+        return list_words
 
+    def parse(self):
+        """
 
+        :return:
+        """
+        # TODO: finish the function
 
 
 
